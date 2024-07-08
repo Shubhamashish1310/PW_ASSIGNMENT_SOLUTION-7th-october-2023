253c3f38f92e0ba0bb7cc4e44b7900410e42d755
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-07-08 15:09:01 +0530

    dev

--- JAVASCRIPT FUNDAMENTAL/ques1.js
+++ JAVASCRIPT FUNDAMENTAL/ques1.js
@@ -1,9 +1,19 @@
-function outerfunction(num1){
+/* Q1. Write a JavaScript function called outerFunction that takes a parameter and returns an inner function.
+ The inner function should access both the parameter of outerFunction and a variable declared within
+ outerFunction. Demonstrate how lexical scoping allows the inner function to maintain access to these
+ variables even after outerFunction has finished executing.*/
 
-    function innerfunction(){
-console.log(num1);
-        return outerfunction()
-    }
-}
 
-outerfunction(innerfunction(                                                                                           ))
+ function outerFunction(a){
+     let b = 10;
+     function innerFunction(){
+         console.log(a,b);
+     }
+     return innerFunction;
+ }
+
+ let shubham =outerFunction(20) //20,10
+ shubham(); //20,10
+
+ //console.log(b);//Uncaught ReferenceError: b is not defined due to lexical scoping
+
